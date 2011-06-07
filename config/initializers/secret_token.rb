a69c485c9a09e3caa937fe143c073afd383514c4
Aleksey Galibin <aleksey@aleksey-VirtualBox.(none)>
2011-06-07 16:36:47 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LinkTrain::Application.config.secret_token = 'b38092bf9efbdccd3b71151d3f325f6f1cf302ee8786c640bb7ae16d87505cb665a5fcb6d80e9556a833f3bcfd09327c24f801097280db871c2a486410dd9926'
